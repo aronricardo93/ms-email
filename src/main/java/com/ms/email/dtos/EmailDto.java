dcbff62a1ce9a0fa350ba8da18c96156bab911e3
Aron Ricardo <aronricardo@hotmail.com>
2022-06-28 14:10:47 -0300

    criacao de alguns beans

--- src/main/java/com/ms/email/dtos/EmailDto.java
+++ src/main/java/com/ms/email/dtos/EmailDto.java
@@ -1,5 +1,14 @@
 package com.ms.email.dtos;
 
+import lombok.Data;
+
+@Data
 public class EmailDto {
 
+	private String ownerRef;
+	private String emailFrom;
+	private String emailTo;
+	private String subject;
+	private String text;
+	
 }
